c51715656c5a0ec396d00ed95876c1213c9668b5
joão Emerson <joaoemersonufc@gmail.com>
2019-09-19 11:52:36 -0300

    Fix errors/Added catch exceptions
    Part of the description is in Portuguese but understandable, anyway i'll change the language to english.

--- Projeto final - Twitter/validar_acesso.php
+++ Projeto final - Twitter/validar_acesso.php
@@ -5,7 +5,7 @@ require_once('db_class.php');
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-$sql = "SELECT * FROM usuario WHERE usuario = '$usuario' AND senha = '$senha'";
+$sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
 
 $objtDB = new db();
 $link = $objtDB->conectaDB();
@@ -14,12 +14,15 @@ $resultado_id = mysqli_query($link, $sql);
 
 if($resultado_id){
 	//Recupera o id e os dados do usuário no BD
-	$dados_usuario = mysqli_fetch($resultado_id);
+	$dados_usuario = mysqli_fetch_array($resultado_id);
+
+	if(isset($dados_usuario['usuario'])){
+		echo 'usuário existe';
+	}else {
+		header('Location: index.php?erro=1');
+	}
 }else{
 	echo 'Erro na execução da consulta, entre em contato com um ADM';
 }
-
-
-var_dump($dados_usuario);
 
 ?>
